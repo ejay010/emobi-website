869abac38e1ae5e5a22726bafa4eb402a61730ff
ejay010 <ejay010@gmail.com>
2018-05-22 20:13:36 -0400

    update to scanner feedback

--- src/components/QrcodeReader.vue
+++ src/components/QrcodeReader.vue
@@ -4,10 +4,21 @@
     Please Scan Ticket
   </div>
   <div class="card-body">
-    <span class="p-1">
-        <qrcode-reader class="card-img" @decode="onDecode" @locate="onLocate"></qrcode-reader>
-      </span>
-    <div class="alert alert-warning" role="alert">
+    <span v-if="cameraLoading">
+      <spinner></spinner>
+    </span>
+    <div class="p-1">
+      <div class="alert alert-warning" role="alert" v-if="found == false">
+        Waiting for E-code...
+      </div>
+      <div class="alert alert-success" role="alert" v-if="found">
+        Found E-code!
+      </div>
+      <qrcode-reader class="card-img" @init="onInit" @decode="onDecode" @locate="onLocate">
+        On stream
+      </qrcode-reader>
+    </div>
+    <div class="alert alert-warning" role="alert" v-if="!cameraOn">
       Please allow e-mobie to access your devices' camera.
     </div>
     <router-link :to="{ name: 'CustomerEventList', params: {} }" class="btn btn-primary">Back to Event Menu</router-link>
@@ -25,12 +36,16 @@ import {
 } from 'vue-qrcode-reader'
 import swal from 'sweetalert2'
 import axios from 'axios'
+import spinner from 'vue-spinner/src/ClipLoader.vue'
 export default {
   components: {
-    QrcodeReader
+    QrcodeReader,
+    spinner
   },
   data: function() {
     return {
+      cameraLoading: true,
+      cameraOn: false,
       found: false
     }
   },
@@ -50,6 +65,34 @@ export default {
     }
   },
   methods: {
+    async onInit(promise) {
+      // show loading indicator
+
+      try {
+        await promise
+        this.cameraLoading = false
+        this.cameraOn = true
+        // successfully initialized
+      } catch (error) {
+        if (error.name === 'NotAllowedError') {
+          // user denied camera access permisson
+        } else if (error.name === 'NotFoundError') {
+          // no suitable camera device installed
+        } else if (error.name === 'NotSupportedError') {
+          // page is not served over HTTPS (or localhost)
+        } else if (error.name === 'NotReadableError') {
+          // maybe camera is already in use
+        } else if (error.name === 'OverconstrainedError') {
+          // passed constraints don't match any camera. Did you requested the front camera although there is none?
+        } else {
+          // browser is probably lacking features (WebRTC, Canvas)
+        }
+      } finally {
+        // hide loading indicator
+        this.cameraLoading = false
+        this.cameraOn = true
+      }
+    },
     onLocate: function(points) {
       if (points.length > 0) {
         this.found = true
@@ -58,8 +101,6 @@ export default {
       }
     },
     onDecode: function(content) {
-      console.log('here is the image content');
-      console.log(content);
       axios.create({
         withCredentials: true
       }).get(process.env.VUE_APP_API_URL + '/purchaseOrder/' + this.$route.params.eventId + '/' + content + '/redeem').then((response) => {
@@ -85,9 +126,22 @@ export default {
               console.log(response);
           }
         } else {
-          console.log('communication error');
+          if (response.data.message == "Redemption Error") {
+            swal({
+              title: response.data.message,
+              text: response.data.error.message,
+              type: 'error'
+            })
+          }
+          // console.log('communication error');
           console.log(response);
         }
+      }).catch((e) => {
+        swal({
+          title: "Sever communication Error",
+          text: e.message,
+          type: 'error'
+        })
       })
     }
   }
