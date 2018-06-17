cccc015b5392d941635f29cb565dc3da6a3d1a58
ejay010 <ejay010@gmail.com>
2018-06-16 22:40:59 -0400

    asdfasd

--- src/components/QrcodeReader.vue
+++ src/components/QrcodeReader.vue
@@ -194,6 +194,11 @@ export default {
     },
     onDecode: function(content) {
       let queryString = content.slice(1).split('&')
+      console.log(queryString);
+      this.$store.dispatch('LogToSlack', {
+        headline: 'testingStringInput',
+        log: queryString
+      })
       let queryObject = {}
       queryString.forEach(function(pair) {
         pair = pair.split('=');
@@ -204,62 +209,62 @@ export default {
         headline: 'QueryString',
         log: result
       })
-      if (result.list != null) {
-        this.$store.dispatch('LogToSlack', {
-          headline: 'QueryObject',
-          log: result
-        })
-      } else {
-        axios.create({
-          withCredentials: true
-        }).get(process.env.VUE_APP_API_URL + '/purchaseOrder/' + this.$route.params.eventId + '/' + result.invoiceId + '/validate').then((response) => {
-          // console.log('post response');
-          // console.log(response.data);
-          this.$store.dispatch('LogToSlack', {
-            headline: 'No query string',
-            log: response.data
-          })
-          if (response.data.success) {
-            switch (response.data.message) {
-              case "Tickets Exhausted":
-                swal({
-                  title: response.data.message,
-                  text: 'This ticket is exhausted :(',
-                  type: 'warning'
-                })
-                break;
-              case "Ticket Found":
-                swal({
-                  title: response.data.message,
-                  text: "Ticket Found, Please Verify Guests",
-                  type: 'success'
-                })
-                this.InvoiceId = response.data.invoice._id
-                this.GuestList = response.data.invoice.contents
-                this.haveGuests = true
-                break;
-              default:
-                console.log(response);
-            }
-          } else {
-            if (response.data.message == "Redemption Error") {
-              swal({
-                title: response.data.message,
-                text: response.data.error.message,
-                type: 'error'
-              })
-            }
-            // console.log('communication error');
-            console.log(response);
-          }
-        }).catch((e) => {
-          swal({
-            title: "Sever communication Error",
-            text: e.message,
-            type: 'error'
-          })
-        })
-      }
+      // if (result.list != null) {
+      //   this.$store.dispatch('LogToSlack', {
+      //     headline: 'QueryObject',
+      //     log: result
+      //   })
+      // } else {
+      //   axios.create({
+      //     withCredentials: true
+      //   }).get(process.env.VUE_APP_API_URL + '/purchaseOrder/' + this.$route.params.eventId + '/' + result.invoiceId + '/validate').then((response) => {
+      //     // console.log('post response');
+      //     // console.log(response.data);
+      //     this.$store.dispatch('LogToSlack', {
+      //       headline: 'No query string',
+      //       log: response.data
+      //     })
+      //     if (response.data.success) {
+      //       switch (response.data.message) {
+      //         case "Tickets Exhausted":
+      //           swal({
+      //             title: response.data.message,
+      //             text: 'This ticket is exhausted :(',
+      //             type: 'warning'
+      //           })
+      //           break;
+      //         case "Ticket Found":
+      //           swal({
+      //             title: response.data.message,
+      //             text: "Ticket Found, Please Verify Guests",
+      //             type: 'success'
+      //           })
+      //           this.InvoiceId = response.data.invoice._id
+      //           this.GuestList = response.data.invoice.contents
+      //           this.haveGuests = true
+      //           break;
+      //         default:
+      //           console.log(response);
+      //       }
+      //     } else {
+      //       if (response.data.message == "Redemption Error") {
+      //         swal({
+      //           title: response.data.message,
+      //           text: response.data.error.message,
+      //           type: 'error'
+      //         })
+      //       }
+      //       // console.log('communication error');
+      //       console.log(response);
+      //     }
+      //   }).catch((e) => {
+      //     swal({
+      //       title: "Sever communication Error",
+      //       text: e.message,
+      //       type: 'error'
+      //     })
+      //   })
+      // }
     }
   }
 }
